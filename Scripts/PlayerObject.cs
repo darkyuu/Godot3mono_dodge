b7149f333951dc58cf40ecfb08ce42406a4f4c39
darkyuu <darkyuu@hotmail.com>
2018-01-12 16:52:02 +0700

    ADD: basic animation for player

---
+++ Scripts/PlayerObject.cs
@@ -0,0 +1,76 @@
+using Godot;
+using System;
+
+public class PlayerObject : Area2D
+{
+    
+    [Export]
+    private int speed = 400;
+    private Vector2 velocity;
+    private Vector2 screenSize;
+    private bool monitoring    
+    
+
+    public AnimatedSprite playerAnimation;
+
+    public override void _Ready()
+    {
+        screenSize = GetViewportRect().Size;
+        playerAnimation = GetNode("AnimatedSprite") as AnimatedSprite;
+    }
+
+    public override void _Process(float delta)
+    {
+        velocity = new Vector2();
+        if (Input.IsActionPressed("ui_right"))
+            velocity.x += 1;
+        if (Input.IsActionPressed("ui_left"))
+            velocity.x -= 1;
+        if (Input.IsActionPressed("ui_down"))
+            velocity.y += 1;
+        if (Input.IsActionPressed("ui_up"))
+            velocity.y -= 1;
+
+        if(velocity.Length() > 0.0f)
+        {
+            playerAnimation.Play();
+            velocity = velocity.Normalized() * speed;
+        }
+        else
+        {
+            playerAnimation.Stop();
+        }
+
+        Position += velocity * delta; 
+        SetPosition(new Vector2(Mathf.Clamp(Position.x, 0 , screenSize.x), Mathf.Clamp(Position.y, 0 , screenSize.y)));
+
+        if (velocity.x != 0)
+        {
+            playerAnimation.Animation = "right";
+            playerAnimation.FlipV = false;
+            playerAnimation.FlipH = velocity.x < 0;
+        }
+        else if (velocity.y != 0)
+        {
+            playerAnimation.Animation = "up";
+            playerAnimation.FlipV = velocity.y > 0;
+        }
+
+
+
+    }
+
+    public void OnPlayerBodyEntered(Area2D area)
+    {
+        Hide();
+        EmitSignal("hit");
+        CallDeferred("set_monitoring", false);
+    }
+
+    public void start(Vector2 pos)
+    {
+        SetPosition(pos);
+        Show();
+        monitoring = true;
+    }
+}
